d4bb29bc5637e3196025f4a8b3c685f992fb4214
Kris Thompson <kt.devcat@gmail.com>
2018-01-06 16:22:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const returnFirstTwoDrivers = function (drivers) {
 }
 
 const returnLastTwoDrivers = function (drivers) {
-  return [drivers [-1], drivers[-2]];
+  return [drivers[-1], drivers[-2]];
 }
