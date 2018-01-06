46bcc7589e2fa6d52aebabf003f6eaadc45967c4
Kris Thompson <kt.devcat@gmail.com>
2018-01-06 16:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ const createFareMultiplier = function (num) {
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, fn) {
+  return fn(drivers);
+}
